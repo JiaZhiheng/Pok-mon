0b1adb6bf083447ac37f69f865bcb987a45e24ba
JiaZhiheng <99702914+JiaZhiheng@users.noreply.github.com>
2022-09-25 22:48:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/pokeapi.png">
+    <Number/>
+    <List/>
+  </div>
+</template>
+
+<script>
+import Number from './components/Number.vue'
+import List from './components/List.vue'
+
+export default {
+  name: 'App',
+  components: { Number, List },
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+  img {
+    display: block;
+    margin: 20px auto;
+  }
+</style>
